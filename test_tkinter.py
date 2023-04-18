8693f5caebc31b548846f8bc738408f8d2f80128
Paul Mareschi <paul.mrsch@gmail.com>
2023-04-18 18:25:00 +0200

    Tests sur Tkinter

--- test_tkinter.py
+++ test_tkinter.py
@@ -1,30 +1,44 @@
-import tkinter
+from tkinter import *
 
-root = tkinter.Tk()
+root = Tk()
 root.title("Hello World")
 root.geometry("500x700")
-label = tkinter.Label(root, text="1\n2\n3\n4\n5\n6")
+
+def bouton():
+    liste.append(label5.cget("text"))
+    suite.config(text=liste)
+
+
+label = Label(root, text="   1\n   2\n   3\n   4\n   5\n   6")
 label.grid(row=0, column=0, sticky="w")
 
-label5 = tkinter.Label(root, text="1")
+label5 = Button(root, text="1", relief = FLAT, command=bouton)
 label5.grid(row=1, column=0, sticky="w")
 
-label2 = tkinter.Label(root, text="2\n3\n4\n5\n6\n7")
+label2 = Label(root, text="   2\n   3\n   4\n   5\n   6\n   7")
 label2.grid(row=0, column=1, sticky="w")
 
-label6 = tkinter.Label(root, text="2")
+label6 = Button(root, text="2", relief = FLAT, command=bouton)
 label6.grid(row=1, column=1, sticky="w")
 
-label3 = tkinter.Label(root, text="3\n4\n5\n6\n7\n8")
+label3 = Label(root, text="   3\n   4\n   5\n   6\n   7\n   8")
 label3.grid(row=0, column=2, sticky="w")
 
-label7 = tkinter.Label(root, text="3")
+label7 = Button(root, text="3", relief = FLAT, command=bouton)
 label7.grid(row=1, column=2, sticky="w")
 
-label4 = tkinter.Label(root, text="4\n5\n6\n7\n8\n9")
+label4 = Label(root, text="   4\n   5\n   6\n   7\n   8\n   9")
 label4.grid(row=0, column=3, sticky="w")
 
-label8 = tkinter.Label(root, text="4")
+label8 = Button(root, text="4", relief = FLAT, command=bouton)
 label8.grid(row=1, column=3, sticky="w")
+
+
+
+liste=[]
+suite = Label(root, text=liste)
+#La suite doit s'afficher en dessous de toutes les colonnes
+suite.grid(row=2, column=0, sticky="nsew")
+
 
 root.mainloop()
