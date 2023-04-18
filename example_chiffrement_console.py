5b7a553289d1615075840494892a1d85310171b5
Paul Mareschi <paul.mrsch@gmail.com>
2023-04-18 17:32:01 +0200

    Fin de l'exemple sur console

--- example_chiffrement_console.py
+++ example_chiffrement_console.py
@@ -1,11 +1,18 @@
 import random
 
+#Déchiffrement de "GRMYSGBOAAMQGDPEYVWLDFDQQQZXXVMSZFS" avec le cylindre et la clé fourni
+
+#Chiffrement et déchiffrement d'un une phrase via une cylindre exemple et une clé fixe
+
+
+
 class chiffrement:
 
-    def __init__(self, nombre_de_cylindre, phrase):
+    def __init__(self, nombre_de_cylindre, phrase, cle):
         self.n = nombre_de_cylindre
         self.phrase = phrase
         self.phrase_chiffré = ""
+        self.cle = cle
 
         #Lecture du fichier : création d'un dictionnaire avec en clé le numéro de la ligne et en valeur la chaine de caractère
         with open ("example_cylinder.txt", "r") as fichier:
@@ -14,7 +21,12 @@ class chiffrement:
                 self.dico[i+1] = ligne
 
         #Génération d'un d'une clé : permutation des entier entre 1 et n 
-        cle = []
+        self.cle = [50, 34, 41, 45, 16, 38, 18, 7, 
+               26, 25, 10, 30, 2, 9, 4, 20, 36, 
+               23, 51, 33, 14, 44, 28, 39, 12, 
+               31, 17, 27, 21, 13, 47, 6, 29, 43, 
+               46, 42, 3, 8, 24, 1, 37, 49, 35, 11, 
+               40, 19, 15, 48, 22, 5, 32]
 
     #chiffrement d'une lettre (lettre située à la position +6 de la chaine de caractère)
     def chiffrement_lettre(self, lettre, cle):
@@ -36,16 +48,7 @@ class chiffrement:
     def return_chiffrement(self):
         return self.phrase_chiffré
 
-phrase = input("Votre phrase : ")
-phrase = phrase.replace(" ", "")
-phrase = phrase.upper()
-n = len(phrase)
-chiffrage = chiffrement(n, phrase)
-print(chiffrage.dico)
-print(chiffrage.cle)
-chiffrage.chiffrement_phrase()
-chiffrage.afficher_chiffrement()
-phrase_chiffree = chiffrage.return_chiffrement()
+
 
 #Déchiffrement
 class dechiffrement:
@@ -79,11 +82,33 @@ class dechiffrement:
 
     def return_dechiffrement(self):
         return self.phrase_dechiffree
-    
 
+
+
+
+#Exemple : chiffrement et déchiffremnt d'une phrase
+phrase = "le grand ours noir danse sur le pont de miami en maillot de bain"
+print("Phrase non chiffrée :\n", phrase, "\n")
+phrase = phrase.replace(" ", "")
+phrase = phrase.upper()
+exemple_cle = [50, 34, 41, 45, 16, 38, 18, 7, 
+               26, 25, 10, 30, 2, 9, 4, 20, 36, 
+               23, 51, 33, 14, 44, 28, 39, 12, 
+               31, 17, 27, 21, 13, 47, 6, 29, 43, 
+               46, 42, 3, 8, 24, 1, 37, 49, 35, 11, 
+               40, 19, 15, 48, 22, 5, 32]
+n = len(phrase)
+chiffrage = chiffrement(n, phrase, exemple_cle)
+chiffrage.chiffrement_phrase()
+
+
+print("Phrase chifrée : ")
+chiffrage.afficher_chiffrement()
+phrase_chiffree = chiffrage.return_chiffrement()
 dechiffrage = dechiffrement(phrase_chiffree, chiffrage.cle)
 dechiffrage._get_dico_()
 dechiffrage.dechiffrement_phrase()
+
+print("\n")
+print("Phrase déchiffrée : ")
 dechiffrage.afficher_dechiffrement()
-phrase_dechiffree = dechiffrage.return_dechiffrement()
-print(phrase_dechiffree)
